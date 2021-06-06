fb415b81307ec4f7e4fbc912b7f0cad0f3fe6797
Ajayi Adeyemi <ajayiadeyemi@gmail.com>
2021-06-06 21:15:32 +0100

    added ng-flex and ng-material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GameboardComponent } from './gameboard/gameboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { GameboardComponent } from './gameboard/gameboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
