7ff2c904384e15dd53c925f9bc8d925058bf5708
Ajayi Adeyemi <ajayiadeyemi@gmail.com>
2021-06-06 21:05:48 +0100

    added game board

---
+++ src/app/gameboard/gameboard.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './gameboard.component.html',
+  styleUrls: ['./gameboard.component.scss']
+})
+export class GameboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
